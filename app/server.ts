d8298405bbdcd0ad5c7c93d5813c7990f40faa5a
jcake <jakieschneider13@gmail.com>
2020-08-06 16:10:18 +1000

    Discarded HTML Transformation for good

--- app/server.ts
+++ app/server.ts
@@ -1,10 +1,12 @@
+import * as path from 'path';
+
 import * as express from 'express';
 
 const app = express();
 
-app.use(express.static(process.cwd() + "/build/final"));
+app.use(express.static(path.join(process.cwd(), "/build/final")));
 
-const port = Number(process.argv[2] || "3500")
+const port = Number(process.argv[2]) || 3500;
 
 app.listen(port, function() {
     console.log('listening on port', port);
