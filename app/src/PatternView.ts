1596a6d769f858cc333b218908ab6f939350e9e0
jcake <jakieschneider13@gmail.com>
2020-08-11 08:17:32 +1000

    Game is working

---
+++ app/src/PatternView.ts
@@ -0,0 +1,13 @@
+import * as p5 from 'p5';
+
+import RenderObject from "./RenderObject";
+
+export default class Pattern extends RenderObject {
+    render(sketch: p5): void {
+
+    }
+
+    protected update(sketch: p5): void {
+    }
+
+}
